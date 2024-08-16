dcd4c3a1e47c4fbbe48e0eeca63a95824c441c49
Lena Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2024-08-16 14:41:41 +0300

    env

--- app/job-details/[id]/page.tsx
+++ app/job-details/[id]/page.tsx
@@ -1,20 +1,14 @@
 import { validateJobType } from "@/types/JobTypeGuard";
 import { Card } from "./card";
+import { OneJob } from "@/service/jobService";
 
 export default async function Details({ params }: { params: { id: string } }) {
-  const { data } = await fetch(
-    `https://jsearch.p.rapidapi.com/job-details?job_id=${params.id}&extended_publisher_details=false`,
-    {
-      headers: {
-        "x-rapidapi-key": "9ff66f0d07msh2e5677f6ea9e1d3p16a33ajsnb2fd88692a26",
-      },
-    }
-  ).then((res) => res.json());
+  const { data } = await OneJob(params.id);
   if (!Array.isArray(data)) {
     throw Error("Wrong data ");
   }
   const job = validateJobType(data[0]);
   if (job) {
-    return <Card job={job}  />;
+    return <Card job={job} />;
   }
 }
--- service/jobService.ts
+++ service/jobService.ts
@@ -3,8 +3,19 @@ export async function AllJobs(search:string) {
         `https://jsearch.p.rapidapi.com/search?query=${search}&date_posted=all`,
         {
           headers: {
-            "x-rapidapi-key": "9ff66f0d07msh2e5677f6ea9e1d3p16a33ajsnb2fd88692a26",
+            "x-rapidapi-key": process.env.API_KEY!,
           },
         }
       ).then((res) => res.json());
 }
+export async function OneJob(id:string) {
+    return await fetch(
+        `https://jsearch.p.rapidapi.com/job-details?job_id=${id}&extended_publisher_details=false`,
+        {
+          headers: {
+            "x-rapidapi-key": process.env.API_KEY!,
+          },
+        }
+      ).then((res) => res.json());
+}
+
